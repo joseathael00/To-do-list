01f67ba695a6ca815bed576bdfd9acf11788b3d2
joseathael00 <joseathael00@gmail.com>
2026-03-16 16:07:41 -0300

    comments

--- scripts/createTask.js
+++ scripts/createTask.js
@@ -1,8 +1,11 @@
+// ? Importa o botão de criar tastks, o container da página, e o input onde o nome da task será digitado
 const createTask = document.querySelector("#create-task");
 const containerDiv = document.getElementById("container");
 const inputNameTask = document.getElementById("task-name-input");
 
 function create() {
+  // ? Cria elementos de div, p e button
+  // ? E aplica estilos css já prontos neles
   const newTask = document.createElement("div");
   const taskName = document.createElement("p");
   const check = document.createElement("button");
@@ -11,11 +14,18 @@ function create() {
   taskName.classList.add("task-name");
   check.classList.add("check");
 
+  // ? Pega o valor do input e o atribui ao nome da nova task
   taskName.textContent = inputNameTask.value;
 
+  // ? Adiciona o elemento div ao container
   containerDiv.appendChild(newTask);
+
+  // ? Adiciona o botão 'chekc' ao elemento div
   newTask.appendChild(check);
+
+  // ? Adiciona o nome da div de task dentro dela
   newTask.appendChild(taskName);
 
+  // ? Limpa o input de nome
   inputNameTask.value = "";
 }
--- scripts/deleteTask.js
+++ scripts/deleteTask.js
@@ -1,6 +1,11 @@
+// ? Importa a div 'container'
 const container = document.getElementById("container");
+
+// ? Cria uma função que ouve o evento de click de uma caixa específica
 container.addEventListener("click", function (event) {
+  // ? Recebe o evento e identifica se a caixa que o clique foi ouvido tem um parente chamado 'check'
   if (event.target.classList.contains("check")) {
+    // ? Captura a exata caixa onde foi ouvido o clique, e a apaga
     const task = event.target.parentElement;
     task.remove();
   }
